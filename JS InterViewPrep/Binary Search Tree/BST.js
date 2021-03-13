faa5d9c7af8ede4bf43a5da9c4566a2fb346857e
johnxferraro <johnxferraro@gmail.com>
2021-03-12 19:25:28 -0500

    implemented BST contains method

--- JS InterViewPrep/Binary Search Tree/BST.js
+++ JS InterViewPrep/Binary Search Tree/BST.js
@@ -6,20 +6,43 @@ class BST {
     }
 
     insert(val) {
+        //instantiate a new BST node with passed in value
         let node = new BST(val);
+        //if the input value is less than the parents value
         if (val < this.val) {
+            //check if there is no left node on the current node
             if (!this.left) {
+                //if there isnt add the new node as this nodes left child
                 this.left = node;
             } else {
+                //otherwise call the insert method on the current nodes left node (recurse);
                 this.left.insert(val);
             }
+            //otherwise that means the input value is greater than the current nodes value
         } else {
+            //if the current node doesnt have a right child
             if (!this.right) {
+                //add the new node as this nodes right child
                 this.right = node;
             } else {
+                //otherwise call the insert method on the current modes right child with the input value
                 this.right.insert(val);
             }
         }
+    }
+
+    contains(val) {
+        if (this.val === val) {
+            return true;
+        }
+        if (val < this.val) {
+            if (!this.left) return false;
+            return this.left.contains(val);
+        } else {
+            if (!this.right) return false;
+            return this.right.contains(val);
+        }
+        return false;
     }
 }
 
@@ -33,5 +56,6 @@ let test = () => {
     root.insert(105)
     root.insert(90)
     console.log(root)
+    console.log(root.contains(107))
 }
 test()
