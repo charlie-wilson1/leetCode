49ebbab14963061721b05fa1aadc47c1c7d8cb69
johnxferraro <johnxferraro@gmail.com>
2021-03-15 10:27:01 -0400

    solved FizzBuzz iterativly

--- JS InterViewPrep/Algos/fizzBuzz.js
+++ JS InterViewPrep/Algos/fizzBuzz.js
@@ -2,28 +2,47 @@
 //if number is divisable by 3 log fizz if 5 log buzz if both lod 'fizzBuzz'
 
 
+// let fizzBuzz = (num) => {
+//     let index = 1;
+
+//     let recurse = (i) => {
+//         if (i === num + 1) {
+//             return;
+//         } else if (i % 15 === 0) {
+//             console.log('FizzBuzz');
+//             return recurse(i + 1)
+//         } else if (i % 5 === 0) {
+//             console.log('Buzz');
+//             return recurse(i + 1)
+//         } else if (i % 3 === 0) {
+//             console.log('Fizz')
+//             return recurse(i + 1)
+//         } else {
+//             console.log(i)
+//             return recurse(i + 1)
+//         }
+
+//     }
+//     recurse(index)
+// }
 let fizzBuzz = (num) => {
     let index = 1;
 
-    let recurse = (i) => {
-        if (i === num + 1) {
-            return;
-        } else if (i % 15 === 0) {
+    while (index <= num) {
+        if (index % 15 === 0) {
             console.log('FizzBuzz');
-            return recurse(i + 1)
-        } else if (i % 5 === 0) {
+            index++;
+        } else if (index % 3 === 0) {
+            console.log('Fizz');
+            index++;
+        } else if (index % 5 === 0) {
             console.log('Buzz');
-            return recurse(i + 1)
-        } else if (i % 3 === 0) {
-            console.log('Fizz')
-            return recurse(i + 1)
+            index++;
         } else {
-            console.log(i)
-            return recurse(i + 1)
+            console.log(index);
+            index++;
         }
-
     }
-    recurse(index)
 }
 
 let test = () => {
