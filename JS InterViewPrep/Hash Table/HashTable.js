20bdc3b4496cbee1c9b41bbd722c0da132de9f7b
johnxferraro <johnxferraro@gmail.com>
2021-03-14 20:17:42 -0400

    implemented hash func and insert method

--- JS InterViewPrep/Hash Table/HashTable.js
+++ JS InterViewPrep/Hash Table/HashTable.js
@@ -2,7 +2,7 @@ class BucketNode {
     constructor(key, val, next) {
         this.key = key;
         this.val = val;
-        this.next = null;
+        this.next = next || null;
     }
 }
 
@@ -11,10 +11,44 @@ class HashTable {
         this.storage = new Array(size);
         this.storageSize = this.storage.length;
     }
+
+    hash(string) {
+        let hashedNum = 0;
+        for (let i = 0; i < string.length; i++) {
+            hashedNum += string.charCodeAt(i);
+        }
+        return (hashedNum % this.storageSize);
+    }
+
+    insert(key, value) {
+        let index = this.hash(key)
+        if (this.storage[index]) {
+            let node = this.storage[index];
+            while (node) {
+                if (node.key === key) {
+                    node.val = value;
+                }
+                if (node.next === null && node.key !== key) {
+                    let next = this.storage[index];
+                    let newNode = new BucketNode(key, value, next);
+                    this.storage[index] = newNode;
+                }
+                node = node.next;
+            }
+        } else {
+            // let nextVal = this.storage[index] ? this.storage[index] : null;
+            let newBucketNode = new BucketNode(key, value);
+            this.storage[index] = newBucketNode;
+        }
+    }
 }
 
 let test = () => {
     let newHT = new HashTable(10);
-    console.log(newHT)
+    newHT.insert('Brian', 'cool');
+    newHT.insert('Brian', 'fast');
+    newHT.insert('here we', 'wang')
+    console.log(newHT.hash('here we'))
+    console.log(newHT.storage)
 }
-test()
+test();
