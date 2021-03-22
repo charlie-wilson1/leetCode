2073930a4fdeb889a9d433fceea466e5b23aede6
johnxferraro <johnxferraro@gmail.com>
2021-03-22 19:30:53 -0400

    added comments to solving of twoSum O(n)

--- JS InterViewPrep/Algos/twoSum.js
+++ JS InterViewPrep/Algos/twoSum.js
@@ -28,13 +28,18 @@ let twoSum = (arr, target) => {
 
     //iterate over th input array
     for(var i = 0; i < arr.length; i++) {
+        //declare current number set to current element in the iteration
         let currNum = arr[i];
+        //declare counterpart set to the input target sum - the curretn number
         let counterPart = target - currNum;
+        //check nif the hashtable array has a number stored at the index of the counter part number
         if (hashtable[counterPart] === counterPart) {
-            console.log(hashtable[counterPart])
+            //if it does then puch the current number and the counter in an aray to the resulkts array
             result.push([currNum, counterPart])
         }
+        //if the hasttable aray does not have anthing stored at it at the index of the current number in the iteration
         if(!hashtable[currNum]) {
+            //add the curretn number to the hash table ant the index === to the current number
             hashtable[currNum] = currNum;
         }
     }
@@ -43,6 +48,6 @@ let twoSum = (arr, target) => {
 
 (function test() {
     let result = twoSum([1,6,4,5,3,3], 7) //[[6,1],[3,4],[3,4]]
-    console.log(JSON.stringify(result))
+    console.log(JSON.stringify(result) === JSON.stringify([[6,1],[3,4],[3,4]]))
     
 })()
